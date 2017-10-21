35d1695ee730d5f3595d9eb95d0b8d3691da8f6a
Alexandru Albu <afalex89@gmail.com>
2017-10-21 18:47:30 +0300

    added react routing setup added react components

---
+++ client-web/imports/routes.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Router, Route} from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+// routes
+import Home from './ui/Home';
+import Authorize from './ui/Authorize';
+import Vote from './ui/Vote';
+
+const browserHistory = createBrowserHistory();
+
+export const renderRoutes = () => (
+  <Router history={browserHistory}>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="authorize" component={Authorize}/>
+      <Route path="vote" component={Vote}/>
+    </div>
+  </Router>
+);
---
+++ client-web/imports/ui/Home.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/authorize">
+          <button>Authorize</button>
+        </Link>
+        <Link to="/vote">
+          <button>Vote</button>
+        </Link>
+      </div>
+    );
+  }
+}
