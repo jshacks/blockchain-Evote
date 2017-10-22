815c6292e4083896c9bbada284e6faf0e0d857e6
Alexandru Albu <afalex89@gmail.com>
2017-10-22 11:38:28 +0300

    home styling

--- client-web/imports/ui/Home.jsx
+++ client-web/imports/ui/Home.jsx
@@ -3,8 +3,13 @@ import {Link} from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
+    const PageStyle = {
+      marginTop : '100px',
+      textAlign : 'center'
+    };
+
     return (
-      <div>
+      <div id="home" style={PageStyle}>
         <Link to="/authorize">
           <button>Authorize</button>
         </Link>
